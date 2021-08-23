7c7871a135546764261a049388664451b2af551e
Joe Hertig <joehertig94@gmail.com>
2021-08-22 20:17:41 -0600

    partially finished Start quiz and high score html, created questions

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+var 
+
+// Questions start //
+var questions = [
+  {
+    q: "Commonly used data types DO NOT include:",
+    o: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    a: "3. alerts",
+  },
+  {
+    q: "The condition in an if / else statement is enclosed within ____.",
+    o: [
+      "1. quotes",
+      "2. curly brackets",
+      "3. parentheses",
+      "4. square brackets",
+    ],
+    a: "3. parentheses",
+  },
+  {
+    q: "Arrays in JavaScript can be used to store _____.",
+    o: [
+      "1. numbers and strings",
+      "2. other arrays",
+      "3. booleans",
+      "4. all of the above",
+    ],
+    a: "4. all of the above",
+  },
+  {
+    q: "String values must be enclosed within _____ when being assigned to variables.",
+    o: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+    a: "3. quotes",
+  },
+  {
+    q: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    o: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+    a: "4. console.log",
+  },
+];
+/* Questions end */
